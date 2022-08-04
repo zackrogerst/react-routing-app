1408e6f55a3f5830f18b6700854f272265b88269
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-04 13:46:48 -0600

    added HighlightedQuote to QuoteDetail

--- src/screens/QuoteDetail.jsx
+++ src/screens/QuoteDetail.jsx
@@ -2,13 +2,26 @@ import React, { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+	{ id: "q1", author: "James", text: "Learning React" },
+	{ id: "q2", author: "John", text: "Lorem Ipsum" },
+	{ id: "q3", author: "Bob", text: "Hello World" }
+];
 
 const QuoteDetail = () => {
 	const params = useParams();
+
+	const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+	if (!quote) {
+		return <p>No Quote Found</p>
+	}
+
 	return (
 		<Fragment>
-			<h1>Quote Detail Page</h1>
-			<p>{params.quoteId}</p>
+			<HighlightedQuote text={quote.text} author={quote.author}/>
 			<Route path={`/quotes/${params.quoteId}/comments`}>
 				<Comments />
 			</Route>
