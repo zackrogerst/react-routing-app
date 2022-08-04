991ed42b327df978c279e92a3fe035a2e6a59d3c
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-04 13:30:37 -0600

    added Layout and MainNavigation components

--- src/screens/QuoteDetail.jsx
+++ src/screens/QuoteDetail.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
-import Comments from '../components/comments/Comments'
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
 	const params = useParams();
@@ -9,9 +9,9 @@ const QuoteDetail = () => {
 		<Fragment>
 			<h1>Quote Detail Page</h1>
 			<p>{params.quoteId}</p>
-            <Route path={`/quotes/${params.quoteId}/comments`}>
-                <Comments />
-            </Route>
+			<Route path={`/quotes/${params.quoteId}/comments`}>
+				<Comments />
+			</Route>
 		</Fragment>
 	);
 };
