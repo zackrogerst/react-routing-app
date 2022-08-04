1a7c8afe90e5bed2853985bff323ac7b037c1153
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-04 16:58:02 -0600

    added load comments button to QuoteDetail

--- src/screens/QuoteDetail.jsx
+++ src/screens/QuoteDetail.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -16,12 +16,19 @@ const QuoteDetail = () => {
 	const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
 
 	if (!quote) {
-		return <p>No Quote Found</p>
+		return <p>No Quote Found</p>;
 	}
 
 	return (
 		<Fragment>
-			<HighlightedQuote text={quote.text} author={quote.author}/>
+			<HighlightedQuote text={quote.text} author={quote.author} />
+			<Route path={`/quotes/${params.quoteId}`} exact>
+				<div className="centered">
+					<Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+						Load Comments
+					</Link>
+				</div>
+			</Route>
 			<Route path={`/quotes/${params.quoteId}/comments`}>
 				<Comments />
 			</Route>
