6f568b15902658d1a288ab684bc23f7d10da3f8e
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-04 13:34:45 -0600

    added QuotesList to AllQuotes

--- src/screens/AllQuotes.jsx
+++ src/screens/AllQuotes.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+	{ id: "q1", author: "James", text: "Learning React" },
+	{ id: "q2", author: "John", text: "Lorem Ipsum" },
+	{ id: "q3", author: "Bob", text: "Hello World" }
+];
 
 const AllQuotes = () => {
-	return (
-		<section>
-			<h1>All Quotes Page</h1>
-		</section>
-	);
+	return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
