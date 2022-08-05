33ca570a5bf9355d45507b07d04bbcf542124578
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-05 11:22:35 -0600

    added comments

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,29 +1,48 @@
-import { useRef } from 'react';
+import React, { useRef, useEffect } from "react";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./NewCommentForm.module.css";
 
-import classes from './NewCommentForm.module.css';
+const NewCommentForm = props => {
+	const commentTextRef = useRef();
 
-const NewCommentForm = (props) => {
-  const commentTextRef = useRef();
+	const { sendRequest, status, error } = useHttp(addComment);
 
-  const submitFormHandler = (event) => {
-    event.preventDefault();
+	const { onAddedComment } = props;
 
-    // optional: Could validate here
+	useEffect(() => {
+		if (status === "completed" && !error) {
+			onAddedComment();
+		}
+	}, [status, error, onAddedComment]);
 
-    // send comment to server
-  };
+	const submitFormHandler = event => {
+		event.preventDefault();
 
-  return (
-    <form className={classes.form} onSubmit={submitFormHandler}>
-      <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
-      </div>
-      <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
-      </div>
-    </form>
-  );
+		const enteredText = commentTextRef.current.value;
+
+		// could validate here
+
+		sendRequest({ commentData: { text: enteredText }, quoteId: props.quoteId });
+	};
+
+	return (
+		<form className={classes.form} onSubmit={submitFormHandler}>
+			{status === "pending" && (
+				<div className="centered">
+					<LoadingSpinner />
+				</div>
+			)}
+			<div className={classes.control} onSubmit={submitFormHandler}>
+				<label htmlFor="comment">Your Comment</label>
+				<textarea id="comment" rows="5" ref={commentTextRef}></textarea>
+			</div>
+			<div className={classes.actions}>
+				<button className="btn">Add Comment</button>
+			</div>
+		</form>
+	);
 };
 
 export default NewCommentForm;
